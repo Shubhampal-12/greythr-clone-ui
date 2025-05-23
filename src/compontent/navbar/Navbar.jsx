16050292d3ec46410286659644195109088326e8
shubham pal <shubham.pal88799@gmail.com>
2025-05-23 19:47:01 +0530

    Create Login Page

--- src/compontent/navbar/Navbar.jsx
+++ src/compontent/navbar/Navbar.jsx
@@ -2,36 +2,39 @@ import React from "react";
 import "./Navbar.css";
 import "../../App.css";
 import image from "./logo.png";
+import { Link } from 'react-router-dom'
+ 
 
 const Navbar = () => {
+  
   return (
-    <nav className="navbar">
+    <nav className="navbar" >
       <div className="navbar-container">
         <a className="navbar-logo">
-          <img src={image} alt="" />
+          <img src={image}  alt="" />
         </a>
 
         <div className="nav-left">
           <div className="">
             <ul className="nav-menu">
               <li className="nav-item">
-                <a className="nav-link">Product</a>
+                <a href="" className="nav-link">Product</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Pricing </a>
+                <a href="" className="nav-link">Pricing </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Customar </a>
+                <a href="" className="nav-link">Customar </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Resources</a>
+                <a href=" " className="nav-link">Resources</a>
               </li>
             </ul>
           </div>
 
           <div className="nav-right">
             <a href="">Request Demo</a>
-            <button>Login</button>
+             <Link to='/login' className="Link">Login</Link>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
- 
- 
+import {BrowserRouter} from 'react-router-dom'
+import Login from './compontent/login_page/Login'
+import Navbar from './compontent/navbar/Navbar'
+
  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+     <BrowserRouter>
+     <App/>
+     </BrowserRouter>
   </StrictMode>,
 )
